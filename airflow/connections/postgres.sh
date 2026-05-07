055c31aad63ca30e6561f037341cad1cb22e9868
saifur213 <ksaifur13@gmail.com>
2026-05-07 11:49:28 +0600

    # Feature   - add connection shell script

---
+++ airflow/connections/postgres.sh
@@ -0,0 +1,7 @@
+airflow connections add postgres_source \
+    --conn-type postgres \
+    --conn-host host.docker.internal \
+    --conn-schema source_db \
+    --conn-login etl_user \
+    --conn-password etluser26 \
+    --conn-port 5432
