a25247214176f8564732bf2ea36f8e8ca73a700a
saifur213 <ksaifur13@gmail.com>
2026-05-14 11:27:06 +0600

    # Feature   - fix dbt dag run issue

--- airflow/dags/dbt_fct_orders_orchestration.py
+++ airflow/dags/dbt_fct_orders_orchestration.py
@@ -1,21 +1,10 @@
 import os
-from dotenv import load_dotenv
-from airflow import DAG
 from cosmos import DbtDag, ProjectConfig, ProfileConfig, ExecutionConfig
 from cosmos.profiles import PostgresUserPasswordProfileMapping
 from config.default_args import DEFAULT_ARGS
 from pathlib import Path
 
 
-# -------------------------
-# Load infra/.env explicitly
-# -------------------------
-load_dotenv(dotenv_path="infra/.env")
-
-
-# -------------------------
-# Postgres Profile Config
-# -------------------------
 profile_config = ProfileConfig(
     profile_name="analytics",
     target_name="postgres_dev",
@@ -25,7 +14,7 @@ profile_config = ProfileConfig(
             "host": "host.docker.internal",
             "port": int(os.getenv("TARGET_POSTGRES_PORT")),
             "dbname": os.getenv("TARGET_POSTGRES_DB"),
-            "schema": "TARGET_POSTGRES_SCHEMA",
+            "schema": os.getenv("TARGET_POSTGRES_SCHEMA"),
             "user": os.getenv("TARGET_POSTGRES_USER"),
             "password": os.getenv("TARGET_POSTGRES_PASSWORD"),
         },
@@ -33,9 +22,6 @@ profile_config = ProfileConfig(
 )
 
 
-# -------------------------
-# Cosmos DBT DAG
-# -------------------------
 dbt_dag = DbtDag(
     dag_id="dbt_fct_orders_pipeline",
 
@@ -58,8 +44,4 @@ dbt_dag = DbtDag(
     max_active_runs=1,
 
     tags=["dbt", "orders", "postgres"],
-
-    operator_args={
-        "dbt_cmd_flags": ["--select", "stg_customers stg_orders fct_orders"]
-    },
 )
